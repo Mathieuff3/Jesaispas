a85141868ead6be1fe719d09a002d7d9e646a5cb
Mathieu <mloire@hotmail.fr>
2021-12-13 14:58:54 +0000

    2nd commit

---
+++ database/migrations/2021_12_13_140624_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+        public function up()
+    {
+        Schema::create('events', function(Blueprint $table) {
+            $table->id();
+            $table->string("name", 100);
+            $table->string("address")->unique();
+            $table->unsignedBigInteger("category_id");
+            $table->foreign("category_id")->on("categories")->references("id");
+            $table->string("duration");
+            $table->string("day");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->on("users")->references("id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
